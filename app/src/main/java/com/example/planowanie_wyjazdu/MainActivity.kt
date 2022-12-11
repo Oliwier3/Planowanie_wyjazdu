afb2495f8d6483cc9cf075aa1749fee4208d3441
Oliwier <oliwier.krauza@gmail.com>
2022-12-11 11:21:38 +0100

    4 commit

--- app/src/main/java/com/example/planowanie_wyjazdu/MainActivity.kt
+++ app/src/main/java/com/example/planowanie_wyjazdu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.planowanie_wyjazdu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 
@@ -10,11 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val x = findViewById<TextView>(R.id.data)
         findViewById<CalendarView>(R.id.Kalendarz).setOnDateChangeListener(CalendarView.OnDateChangeListener { _, year, month, dayOfMonth ->
-            val data =  dayOfMonth.toString() + "-" + month+1 + "-" + year
-            findViewById<TextView>(R.id.data).text = data
-
+            val data =  dayOfMonth.toString() + "-" + (month+1) + "-" + year
+            x.text = data
         })
+        findViewById<Button>(R.id.wyjazd).setOnClickListener {
+            findViewById<TextView>(R.id.dataWyjazd).text = x.text
+        }
+
+        findViewById<Button>(R.id.powrot).setOnClickListener {
+            findViewById<TextView>(R.id.dataPowrot).text = x.text
+        }
+
+        findViewById<Button>(R.id.Oblicz).setOnClickListener {
+            val a = findViewById<TextView>(R.id.dataPowrot).text.toString().toInt()
+            val b = findViewById<TextView>(R.id.dataWyjazd).text.toString().toInt()
+            var c = b-a
+            findViewById<TextView>(R.id.Wynik).text = c.toString()
+        }
     }
 }
