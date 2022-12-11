e5be3a02d577331426d1296b9d1c26613bb93855
Oliwier <oliwier.krauza@gmail.com>
2022-12-11 10:47:04 +0100

    2 commit

--- app/src/main/java/com/example/planowanie_wyjazdu/MainActivity.kt
+++ app/src/main/java/com/example/planowanie_wyjazdu/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.planowanie_wyjazdu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var dataWidok = findViewById<TextView>(R.id.data).text.toString()
+        findViewById<CalendarView>(R.id.Kalendarz).setOnDateChangeListener(CalendarView.OnDateChangeListener { _, year, month, dayOfMonth ->
+            val data = dayOfMonth.toString() + "-" + month + "-" + year
+            dataWidok = data
+        })
     }
 }
